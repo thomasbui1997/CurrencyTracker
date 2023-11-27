d4bed96ff7e4d8c4ce49ccdb6dfa6876276cd6a3
Thomas Bui <thomasbui@Suzys-Mac-mini.local>
2023-11-27 11:41:59 -0500

    Separated API Call from main function

--- Main.cpp
+++ Main.cpp
@@ -10,18 +10,16 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* out
     return total_size;
 }
 
-int main() 
+
+json makeApiCall(const char* url) 
 {
     CURL* curl;
     CURLcode res;
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
     string response {};
-    json jsonData {};
-    float exchangeRate {};
 
     if (curl) {
-        const char* url = "https://openexchangerates.org/api/latest.json?app_id=91f635eec4b947fa8a7d1c5db6a5d02d";
         curl_easy_setopt(curl, CURLOPT_URL, url);
 
         // Setup URL to fetch, handle response
@@ -36,14 +34,8 @@ int main()
         } else {
             // SUCCESS API CALL
             try {
-                jsonData = json::parse(response);
-                //std::cout << response << std::endl;
-
-                std::cout << "Base currency: " << jsonData["base"] << std::endl;
-                const auto& rates = jsonData["rates"];
-                exchangeRate = jsonData["rates"]["EUR"].get<float>();
-                std::cout << "USD to EUR exchange rates: " << rates["EUR"] << std::endl;
-
+                return json::parse(response);
+                
             } catch (const json::exception& e) {
                 std::cerr << "JSON parsing error: " << e.what() << std::endl;
             }
@@ -54,11 +46,26 @@ int main()
         std::cerr << "Failed to initialize cURL. " << std::endl;
     }
     curl_global_cleanup();
+    return json();
+}
+
+
+int main() 
+{
+    string response {};
+    float exchangeRate {};
+
+    const char* url {"https://openexchangerates.org/api/latest.json?app_id=91f635eec4b947fa8a7d1c5db6a5d02d"};
+    json jsonData { makeApiCall(url) };
+
+    std::cout << "Base currency: " << jsonData[0]["base"] << std::endl;
+    const auto& rates = jsonData[0]["rates"];
+    exchangeRate = jsonData[0]["rates"]["EUR"].get<float>();
+    std::cout << "USD to EUR exchange rates: " << rates["EUR"] << std::endl;
 
 
     // Display information
     float euros { getUserInput("How many euros are you exchanging? ") };
-    //float exchangeRate { getUserInput("What is the exchange rate? ") };
 
     const float euroRate = 1 / exchangeRate;
     float dollars = euros * euroRate;
